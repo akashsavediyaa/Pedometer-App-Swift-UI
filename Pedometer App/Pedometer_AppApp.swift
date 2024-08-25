5029a2270c1df036ce773b773f9b3b13f99f4008
akashsavediyaa <akashsavediya@gmail.com>
2024-08-26 04:08:46 +0530

    Initial Commit

---
+++ Pedometer App/Pedometer_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pedometer_AppApp.swift
+//  Pedometer App
+//
+//  Created by Akash Savediya on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Pedometer_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
